93253f2d92fc9c73e88e25db161e3ab8b707d445
Florin Lazau <florin.lazau@lateral-inc.com>
2017-09-21 15:36:52 +0300

    This is new version

---
+++ mygetpack/Build.ps1
@@ -0,0 +1,5 @@
+﻿if(Test-Path .\nupkgs) { Remove-Item .\nupkgs -Force -Recurse }
+
+dotnet build 
+
+ dotnet pack /p:PackageVersion=2.0.3.2   --output nupkgs
--- mygetpack/Class1.cs
+++ mygetpack/Class1.cs
@@ -4,5 +4,9 @@ namespace mygetpack
 {
     public class Class1
     {
+        public string OneString { get; set; }
+        public string OneStdsdssring { get; set; }
+        public string OneStdsddsdssring { get; set; }
+        public string OneStdsddsdsdssring { get; set; }
     }
 }
