29779342a8c1b5043df8c767315ed2e8db01848f
Florin Lazau <florin.lazau@lateral-inc.com>
2017-09-21 17:03:04 +0300

    new way

--- mygetpack/Build.ps1
+++ mygetpack/Build.ps1
@@ -1,5 +1,16 @@
 ﻿if(Test-Path .\nupkgs) { Remove-Item .\nupkgs -Force -Recurse }
 
+$myBuildNumber = (Get-Date).Year + ((Get-Date).Month) + (Get-Date).Day +(Get-Date).Hour+(Get-Date).Minute   +(Get-Date).Second  
+
+$filePath =  Resolve-Path  "mygetpack.csproj"
+$xml=New-Object XML
+$xml.Load($filePath)
+
+$splitNumber = $xml.Project.PropertyGroup.Version.Split(".")
+$majorNumber = $splitNumber[0]
+$minorNumber = $splitNumber[1]
+$patchNumber = $splitNumber[2]
+
 dotnet build 
 
- dotnet pack /p:PackageVersion=2.0.3.2   --output nupkgs
+dotnet pack /p:PackageVersion=$majorNumber.$minorNumber.$patchNumber.$myBuildNumber   --output nupkgs
